375b6b7b278fa0c231a76ee43b44bbea7143f5c5
eguegan <e.guegan@margulls.com>
2016-04-01 11:32:43 -0400

    Add backgroud notification project

---
+++ app/src/main/java/com/example/admin/backgroundnotifcation/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.admin.backgroundnotifcation;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        notify("Activity is on Background");
+    }
+
+    private void notify(String methodName) {
+        String name = this.getClass().getName();
+        String[] strings = name.split("\\.");
+        Notification noti = new Notification.Builder(this)
+                .setContentTitle(methodName + " " + strings[strings.length - 1]).setAutoCancel(true)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentText(name).build();
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify((int) System.currentTimeMillis(), noti);
+    }
+}
